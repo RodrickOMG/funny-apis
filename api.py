f8ba638876e177592303b40f33472e9e7783ee67
RodrickOMG <206552@qq.com>
2024-05-13 11:28:58 +0800

    update dress recommend api

--- api.py
+++ api.py
@@ -17,27 +17,26 @@ def dress_recommend():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:120.0) Gecko/20100101 Firefox/120.0',
     }
-    city = request.args.get('city', 'chongqing')  # 默认值为'chongqing'
-    try:
-        # 获取字符串格式的html_doc。由于content为bytes类型，故需要decode()
-        html_doc = requests.get('https://www.tianqi.com/chuanyi-'+city+'.html', headers=headers).content.decode()
-        # 使用BeautifulSoup模块对页面文件进行解析
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        # 查找所有class为'detail_life_zhishu'的ul元素中的li元素
-        li_elements = soup.find('ul', class_='detail_life_zhishu').find_all('li')
-    except AttributeError:
-        html_doc = requests.get('https://www.tianqi.com/chuanyi-chongqing.html', headers=headers).content.decode()
-        # 使用BeautifulSoup模块对页面文件进行解析
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        # 查找所有class为'detail_life_zhishu'的ul元素中的li元素
-        li_elements = soup.find('ul', class_='detail_life_zhishu').find_all('li')
-    # 遍历每个li元素，提取和打印信息
-    for li in li_elements:
-        # 提取指数名称和建议
-        if '穿衣指数' in li.find('h2').text.strip():
-            status = li.find('span').text.strip()
-            suggestion = li.find('p').text.strip()
-            return f"{status}，{suggestion}"
+    city = request.args.get('city', 'dalian')  # 默认值为'dalian'
+    county_id = 101070201
+    word = '大连'
+    if city == 'dalian':
+        county_id = 101070201
+        word = '大连'
+    elif city == 'chongqing':
+        county_id = 101040100
+        word = '重庆'
+    # 获取字符串格式的html_doc。由于content为bytes类型，故需要decode()
+    html_doc = requests.get('https://m.baidu.com/sf?county_id='+str(county_id)+'&dspName=iphone&ext=%7B%22bar_'
+                            'sort%22%3A%22chuanyi%2Cchuyou%2Cxiche%2Chuazhuang%2Cganmao%2Cfangshai%2C%22%2C%22sf_'
+                            'tab_name%22%3A%22chuanyi%22%7D&from_sf=1&fromapp=vsgo&openapi=1&pd=life_compare_weather'
+                            '&resource_id=4599&title=生活气象指数&word='+word+'&fromSite=pc', headers=headers).content.decode()
+    # 使用BeautifulSoup模块对页面文件进行解析
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    # 查找所有class为'detail_life_zhishu'的ul元素中的li元素
+    text = soup.find('p', class_='sfc-weather-number-today-desc c-color-gray-a c-gap-top')
+    print(str(text))
+    return str(text)
 
 
 if __name__ == '__main__':
