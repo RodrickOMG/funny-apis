293e6e6c392d71f9edab65bb41652a6c03958ba1
RodrickOMG <206552@qq.com>
2024-09-02 15:40:33 +0800

    add exception handle

--- api.py
+++ api.py
@@ -6,11 +6,12 @@
 @Author  ：Rodrick
 @Date    ：2024/2/23 15:30 
 '''
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
 
 @app.route('/funny-apis/dress-recommend')
 def dress_recommend():
@@ -18,46 +19,50 @@ def dress_recommend():
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:120.0) Gecko/20100101 Firefox/120.0',
     }
     city = request.args.get('city', 'dalian')  # 默认值为'dalian'
-    county_id = 101070201
-    word = '大连'
     if city == 'dalian':
         county_id = 101070201
         word = '大连'
     elif city == 'chongqing':
         county_id = 101040100
         word = '重庆'
-    # 获取字符串格式的html_doc。由于content为bytes类型，故需要decode()
-    html_doc = requests.get('https://m.baidu.com/sf?county_id='+str(county_id)+'&dspName=iphone&ext=%7B%22bar_'
-                            'sort%22%3A%22chuanyi%2Cchuyou%2Cxiche%2Chuazhuang%2Cganmao%2Cfangshai%2C%22%2C%22sf_'
-                            'tab_name%22%3A%22chuanyi%22%7D&from_sf=1&fromapp=vsgo&openapi=1&pd=life_compare_weather'
-                            '&resource_id=4599&title=生活气象指数&word='+word+'&fromSite=pc', headers=headers).content.decode()
-    # 使用BeautifulSoup模块对页面文件进行解析
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    # 查找所有class为'detail_life_zhishu'的ul元素中的li元素
-    text = soup.find('p', class_='sfc-weather-number-today-desc c-color-gray-a c-gap-top')
-    print(str(text))
-    return str(text)
+    else:
+        return "City not supported", 400
+
+    try:
+        response = requests.get(
+            f'https://m.baidu.com/sf?county_id={county_id}&dspName=iphone&ext=%7B%22bar_sort%22%3A%22chuanyi%2Cchuyou%2Cxiche%2Chuazhuang%2Cganmao%2Cfangshai%2C%22%2C%22sf_tab_name%22%3A%22chuanyi%22%7D&from_sf=1&fromapp=vsgo&openapi=1&pd=life_compare_weather&resource_id=4599&title=生活气象指数&word={word}&fromSite=pc',
+            headers=headers
+        )
+        response.raise_for_status()
+        html_doc = response.content.decode()
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        text = soup.find('p', class_='sfc-weather-number-today-desc c-color-gray-a c-gap-top')
+        return str(text) if text else "No data available"
+    except requests.RequestException as e:
+        return str(e), 500
+
 
 @app.route('/funny-apis/daily-english')
 def daily_english():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:120.0) Gecko/20100101 Firefox/120.0',
     }
-    url = "https://apis.tianapi.com/everyday/index"  # 替换为你要发送请求的 URL
+    url = "https://apis.tianapi.com/everyday/index"
     params = {
-        "key": "ae3ebd6c8e2b1fbe3656f67996a6a26f"
+        "key": "your_api_key_here"  # 使用环境变量管理API密钥
     }
 
-    # 发送带参数的 GET 请求
-    response = requests.get(url, params=params, headers=headers)
-    # 检查响应状态码
-    if response.status_code == 200:
-        # 打印响应内容
-        json = response.json()
-        return json['result']['content']+' '+json['result']['note']
-    else:
-        return "请求失败，状态码：", response.status_code
+    try:
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+        json_data = response.json()
+        return jsonify({
+            'content': json_data['result']['content'],
+            'note': json_data['result']['note']
+        })
+    except requests.RequestException as e:
+        return str(e), 500
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=18000)
